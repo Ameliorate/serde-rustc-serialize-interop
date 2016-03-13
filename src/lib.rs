f31e765f9bbfa1480a3fa12b430bce5120aedc72
Amelorate <ameilorate2@gmail.com>
2016-03-12 22:07:31 -0500

    Add rustc-serialize intercompatibility

--- src/lib.rs
+++ src/lib.rs
@@ -14,29 +14,31 @@
 #[cfg(test)]
 mod test;
 
+extern crate bincode;
+extern crate either;
 extern crate serde;
 extern crate rustc_serialize;
-extern crate bincode;
 
-use std::io;
-
+use bincode::rustc_serialize::{DecodingError, decode, encode};
 use bincode::serde::{DeserializeError, SerializeError, deserialize, serialize};
 use bincode::SizeLimit;
+use either::Either;
+use rustc_serialize::{Encodable, Decodable};
 use serde::{Deserialize, Serialize};
 use serde::de::value::Error as SerdeError;
 
 /// The serialization mid-end the Interop struct was originally made from.
-#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
+#[derive(Serialize, Deserialize, RustcDecodable, RustcEncodable, Clone, Copy, Debug)]
 pub enum Origin {
     /// The Interop was constructed using serde.
     Serde,
+    /// The interop was constructed using rustc-serialize.
+    RustcSerialize,
 }
 
 /// Represents an error that hapened during serialization/deserialization.
 #[derive(Debug)]
 pub enum Error {
-    /// An error originating from the underlying Writer.
-    IoError(io::Error),
     /// A custom error message.
     Custom(String),
     /// An internal serde error.
@@ -45,14 +47,14 @@ pub enum Error {
     ///
     /// This contains a DeserializeError from bincode, because the struct in bincode that normally represents this value is private.
     /// It is however, gaurenteed that this will only ever contain DeserializeError::InvalidEncoding.
-    InvalidEncoding(DeserializeError),
+    InvalidEncoding(Either<DeserializeError, DecodingError>),
 }
 
 impl From<SerializeError> for Error {
     fn from(from: SerializeError) -> Self {
         match from {
             SerializeError::Custom(err) => Error::Custom(err),
-            SerializeError::IoError(err) => Error::IoError(err),
+            SerializeError::IoError(_) => panic!("Attempt to convert SerializeError::IoError to Error"),
             SerializeError::SizeLimit => panic!("Attempt to convert SerializeError::SizeLimit to Error"),
         }
     }
@@ -61,16 +63,26 @@ impl From<SerializeError> for Error {
 impl From<DeserializeError> for Error {
     fn from(from: DeserializeError) -> Self {
         match from {
-            DeserializeError::IoError(err) => Error::IoError(err),
-            DeserializeError::InvalidEncoding(_) => Error::InvalidEncoding(from),
+            DeserializeError::IoError(_) => panic!("Attempt to convert SerializeError::IoError to Error"),
+            DeserializeError::InvalidEncoding(_) => Error::InvalidEncoding(Either::Left(from)),
             DeserializeError::SizeLimit => panic!("Attempt to convert SerializeError::SizeLimit to Error"),
             DeserializeError::Serde(err) => Error::Serde(err),
         }
     }
 }
 
+impl From<DecodingError> for Error {
+    fn from(from: DecodingError) -> Self {
+        match from {
+            DecodingError::InvalidEncoding(_) => Error::InvalidEncoding(Either::Right(from)),
+            DecodingError::IoError(_) => panic!("Attempt to convert DecodingError::IoError to Error"),
+            DecodingError::SizeLimit => panic!("Attempt to convert DecodingError::SizeLimit to Error"),
+        }
+    }
+}
+
 /// Allows encoding a struct implementing serde's Serialize using rustc_serialize, or vice versa.
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, RustcDecodable, RustcEncodable, Debug)]
 pub struct Interop {
     repr: Vec<u8>,
     /// The serialization mid-end the Interop struct was originally made from.
@@ -93,6 +105,26 @@ impl Interop {
     pub fn serde_deser<T: Deserialize>(&self) -> Result<T, Error> {
         Ok(match self.origin {
             Origin::Serde => try!(deserialize(&self.repr)),
+            Origin::RustcSerialize => panic!("Called serde_deser on value constructed using rustc-serialize"),
+        })
+    }
+
+    /// Creates an Interop struct using rustc-serialize.
+    pub fn rustc<T: Encodable>(from: &T) -> Result<Self, Error> {
+        Ok(Interop {
+            repr: encode(from, SizeLimit::Infinite).expect("Assertion error: encode returned err value with SizeLimit::Infinite"),
+            origin: Origin::RustcSerialize,
+        })
+    }
+
+    /// Deserializes the Interop into it's origin value.
+    ///
+    /// # Panics
+    /// * Calling on a Interop that was not constructed using the rustc-serialize method.
+    pub fn rustc_deser<T: Decodable>(&self) -> Result<T, Error> {
+        Ok(match self.origin {
+            Origin::Serde => panic!("Called rustc_deser on value constructed using serde"),
+            Origin::RustcSerialize => try!(decode(&self.repr)),
         })
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,38 @@
 use super::*;
 
-#[derive(Serialize, Deserialize, Clone, Copy, Debug, Eq, PartialEq)]
-struct TestStruct;
+#[derive(Serialize, Deserialize, RustcDecodable, RustcEncodable, Clone, Copy, Debug, Eq, PartialEq)]
+struct TestStruct(u32);
 
 #[test]
 fn serde_to_serde() {
-    let test = TestStruct;
+    let test = TestStruct(238524);
     let interop = Interop::serde(&test).unwrap();
     let test_deser: TestStruct = interop.serde_deser().unwrap();
+    assert_eq!(test, test_deser);
+}
+
+#[test]
+#[should_panic(expected = "Called serde_deser on value constructed using rustc-serialize")]
+fn serde_to_rustc() {
+    let test = TestStruct(2305349807);
+    let interop = Interop::rustc(&test).unwrap();
+    let test_deser: TestStruct = interop.serde_deser().unwrap();
+    assert_eq!(test, test_deser);
+}
+
+#[test]
+fn rustc_to_rustc() {
+    let test = TestStruct(2568913);
+    let interop = Interop::rustc(&test).unwrap();
+    let test_deser: TestStruct = interop.rustc_deser().unwrap();
+    assert_eq!(test, test_deser);
+}
+
+#[test]
+#[should_panic(expected = "Called rustc_deser on value constructed using serde")]
+fn rustc_to_serde() {
+    let test = TestStruct(23537566);
+    let interop = Interop::serde(&test).unwrap();
+    let test_deser: TestStruct = interop.rustc_deser().unwrap();
     assert_eq!(test, test_deser);
 }
